fb3c640d76b8473dec7cc23215520ee6d7ac91fc
Keith Craigie <keith@cloudengines.com>
2015-08-31 15:47:52 -0700

    Extending from android.app.activity to get proper auto back stack behavior

--- app/src/main/java/com/kcraigie/flashcards/MainActivity.java
+++ app/src/main/java/com/kcraigie/flashcards/MainActivity.java
@@ -1,9 +1,10 @@
 package com.kcraigie.flashcards;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.app.FragmentTransaction;
 
-public class MainActivity extends android.support.v7.app.AppCompatActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
